78b413268f162ef0d8d602bb984616a7b9d5e372
Richard Hopton <richard@thehoptons.com>
2023-12-28 01:49:41 -0800

    (HomeAssistant) Add number entity type

---
+++ src/HomeAssistant/NumberSlider.test.ts
@@ -0,0 +1,181 @@
+import { IMQTTConnection } from '@mqtt/IMQTTConnection';
+import { mocked, testDevice } from '@utils/testHelpers';
+import { mock } from 'jest-mock-extended';
+import { NumberSlider, NumberSliderConfig } from './NumberSlider';
+
+const mqtt: IMQTTConnection = mock<IMQTTConnection>();
+const onChange = jest.fn();
+const buildSubject = (config: { category?: string } & NumberSliderConfig = {}) =>
+  new NumberSlider(mqtt, testDevice, { description: 'Number Slider', ...config }, async (state) => {
+    onChange(state);
+  });
+
+describe(NumberSlider.name, () => {
+  beforeAll(() => jest.useFakeTimers());
+
+  beforeEach(jest.resetAllMocks);
+
+  describe('publishes discovery', () => {
+    let onFunc: ((state: string) => Promise<void>) | null = null;
+
+    beforeEach(() => {
+      onFunc = null;
+      mocked(mqtt.on).mockImplementation((topic, func) => {
+        if (topic === 'homeassistant/status') onFunc = func;
+      });
+    });
+
+    it('on construction', () => {
+      buildSubject();
+      jest.runAllTimers();
+      expect(mqtt.publish).toBeCalledWith('homeassistant/number/device_topic_number_slider/config', {
+        availability_topic: 'device_topic/number_slider/status',
+        device: { ...testDevice.device },
+        min: 0,
+        max: 100,
+        mode: 'slider',
+        name: 'Number Slider',
+        payload_available: 'online',
+        payload_not_available: 'offline',
+        state_topic: 'device_topic/number_slider/state',
+        unique_id: 'test_name_number_slider',
+        command_topic: 'device_topic/number_slider/command',
+      });
+    });
+
+    it('on construction with entity category', () => {
+      buildSubject({ category: 'config' });
+      jest.runAllTimers();
+      expect(mqtt.publish).toBeCalledWith('homeassistant/number/device_topic_number_slider/config', {
+        availability_topic: 'device_topic/number_slider/status',
+        device: { ...testDevice.device },
+        min: 0,
+        max: 100,
+        mode: 'slider',
+        name: 'Number Slider',
+        payload_available: 'online',
+        payload_not_available: 'offline',
+        state_topic: 'device_topic/number_slider/state',
+        unique_id: 'test_name_number_slider',
+        command_topic: 'device_topic/number_slider/command',
+        entity_category: 'config',
+      });
+    });
+
+    it('on construction with config', () => {
+      buildSubject({ min: 1, max: 64, icon: 'mdi:waves-arrow-right' });
+      jest.runAllTimers();
+      expect(mqtt.publish).toBeCalledWith('homeassistant/number/device_topic_number_slider/config', {
+        availability_topic: 'device_topic/number_slider/status',
+        device: { ...testDevice.device },
+        icon: 'mdi:waves-arrow-right',
+        min: 1,
+        max: 64,
+        mode: 'slider',
+        name: 'Number Slider',
+        payload_available: 'online',
+        payload_not_available: 'offline',
+        state_topic: 'device_topic/number_slider/state',
+        unique_id: 'test_name_number_slider',
+        command_topic: 'device_topic/number_slider/command',
+      });
+    });
+
+    it('when status online is receieved', () => {
+      buildSubject();
+      jest.runAllTimers();
+      expect(onFunc).not.toBeNull();
+      if (!onFunc) return;
+
+      jest.resetAllMocks();
+      onFunc('online');
+      jest.runAllTimers();
+      expect(mqtt.publish).toBeCalledWith('homeassistant/number/device_topic_number_slider/config', {
+        availability_topic: 'device_topic/number_slider/status',
+        device: { ...testDevice.device },
+        min: 0,
+        max: 100,
+        mode: 'slider',
+        name: 'Number Slider',
+        payload_available: 'online',
+        payload_not_available: 'offline',
+        state_topic: 'device_topic/number_slider/state',
+        unique_id: 'test_name_number_slider',
+        command_topic: 'device_topic/number_slider/command',
+      });
+    });
+  });
+
+  it('subscribes to command on construction', () => {
+    buildSubject();
+    expect(mqtt.subscribe).toBeCalledWith('device_topic/number_slider/command');
+  });
+
+  it('attaches event emitter on construction', () => {
+    (mqtt.on as jest.Mock<any, any>).mockImplementationOnce((_command, _func) => {
+      // noop
+    });
+    buildSubject();
+    expect(mqtt.on).toBeCalledWith('device_topic/number_slider/command', expect.anything());
+  });
+
+  describe('command handling', () => {
+    let onFunc: ((state: string) => Promise<void>) | null = null;
+
+    beforeEach(() => {
+      onFunc = null;
+      mocked(mqtt.on).mockImplementation((topic, func) => {
+        if (topic !== 'homeassistant/status') onFunc = func;
+      });
+    });
+
+    it.each([
+      ['2', 2, '2'],
+      ['20', 20, '20'],
+    ])('published state %s when event handler is called', async (payload, state, expectedPublish) => {
+      buildSubject();
+      expect(mqtt.publish).not.toBeCalledWith('device_topic/number_slider/state');
+      expect(onFunc).not.toBeNull();
+      if (!onFunc) return;
+
+      jest.resetAllMocks();
+      onChange.mockImplementation(() => {});
+      await onFunc(payload);
+      expect(onChange).toBeCalledWith(state);
+      jest.runAllTimers();
+      expect(mqtt.publish).toBeCalledWith('device_topic/number_slider/state', expectedPublish);
+    });
+
+    it('ignores unexpected payloads', () => {
+      buildSubject();
+      expect(mqtt.publish).not.toBeCalledWith('device_topic/number_slider/state');
+      expect(onFunc).not.toBeNull();
+      if (!onFunc) return;
+
+      jest.resetAllMocks();
+      onFunc('UNSUPPORTED');
+      expect(onChange).not.toBeCalled();
+      jest.runAllTimers();
+      expect(mqtt.publish).not.toBeCalledWith('device_topic/number_slider/state', expect.anything());
+    });
+  });
+  describe('call setState', () => {
+    const entity = buildSubject();
+
+    it.each([
+      ['5', 5],
+      ['10', 10],
+    ])('publishes state %s when setState called with %s', (expected, state) => {
+      entity.setState(state);
+      jest.runAllTimers();
+      expect(mqtt.publish).toBeCalledWith('device_topic/number_slider/state', expected);
+    });
+
+    it('publishes available offline when setState called with null', () => {
+      entity.setState(null);
+      jest.runAllTimers();
+      expect(mqtt.publish).toBeCalledWith('device_topic/number_slider/status', 'offline');
+      expect(mqtt.publish).not.toBeCalledWith('device_topic/number_slider/state', null);
+    });
+  });
+});
---
+++ src/HomeAssistant/NumberSlider.ts
@@ -0,0 +1,53 @@
+import { IMQTTConnection } from '@mqtt/IMQTTConnection';
+import { IDeviceData } from './IDeviceData';
+import { EntityConfig } from './base/Entity';
+import { StatefulEntity } from './base/StatefulEntity';
+
+export type NumberSliderConfig = {
+  min?: number;
+  max?: number;
+  icon?: string;
+};
+
+export class NumberSlider extends StatefulEntity<number> {
+  private commandTopic: string;
+  private min: number;
+  private max: number;
+  private icon?: string;
+
+  constructor(
+    mqtt: IMQTTConnection,
+    deviceData: IDeviceData,
+    { min = 0, max = 100, icon, ...config }: EntityConfig & NumberSliderConfig,
+    onChange: (state: number) => Promise<void | number>
+  ) {
+    super(mqtt, deviceData, config, 'number');
+    this.min = min;
+    this.max = max;
+    this.icon = icon;
+    this.commandTopic = `${this.baseTopic}/command`;
+
+    mqtt.subscribe(this.commandTopic);
+    mqtt.on(this.commandTopic, async (message) => {
+      const value = parseInt(message);
+      if (Number.isNaN(value)) return;
+      let result = await onChange(value);
+      this.setState(result === undefined ? value : result);
+    });
+  }
+
+  mapState(state?: number): string | null {
+    return state == undefined ? null : state.toString();
+  }
+
+  discoveryState() {
+    return {
+      ...super.discoveryState(),
+      command_topic: this.commandTopic,
+      mode: 'slider',
+      icon: this.icon,
+      min: this.min,
+      max: this.max,
+    };
+  }
+}
