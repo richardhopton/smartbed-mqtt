aec99e66d9c84e566a927d63e47e1c8e66e26001
Richard Hopton <richard@thehoptons.com>
2024-02-28 09:54:56 -0800

    (ESPHome) Remove defunct discovery logic

--- src/ESPHome/connectToESPHome.ts
+++ src/ESPHome/connectToESPHome.ts
@@ -1,23 +1,22 @@
 import { Connection } from '@2colors/esphome-native-api';
 import { logInfo } from '@utils/logger';
-import { connect } from './connect';
-import { discoverProxies } from './discoverProxies';
 import { ESPConnection } from './ESPConnection';
 import { IESPConnection } from './IESPConnection';
+import { connect } from './connect';
 import { BLEProxy, getProxies } from './options';
 
 export const connectToESPHome = async (): Promise<IESPConnection> => {
   logInfo('[ESPHome] Connecting...');
 
   const proxies = getProxies();
-  const useDiscovery = proxies.length === 1 && proxies[0].host === '<discover>';
-  const connections = useDiscovery
-    ? await discoverProxies(proxies[0].password || '')
-    : await Promise.all(
-        proxies.map(async (config: BLEProxy) => {
-          const connection = new Connection(config);
-          return await connect(connection);
-        })
-      );
+  const connections =
+    proxies.length == 0
+      ? []
+      : await Promise.all(
+          proxies.map(async (config: BLEProxy) => {
+            const connection = new Connection(config);
+            return await connect(connection);
+          })
+        );
   return new ESPConnection(connections);
 };
