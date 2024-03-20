0449c16e43bb4d6ba31a3112fadecdaf397ea6f7
Richard Hopton <richard@thehoptons.com>
2024-03-19 22:37:25 -0700

    Fix bad message context

--- src/Reverie/setupPresetButtons.ts
+++ src/Reverie/setupPresetButtons.ts
@@ -4,17 +4,17 @@ import { Commands } from './Commands';
 import { Controller } from './Controller';
 
 export const setupPresetButtons = (mqtt: IMQTTConnection, controller: Controller) => {
-  buildCachedButton('Richmat', mqtt, controller, 'PresetZeroG', Commands.PresetZeroG);
-  buildCachedButton('Richmat', mqtt, controller, 'PresetAntiSnore', Commands.PresetAntiSnore);
-  buildCachedButton('Richmat', mqtt, controller, 'PresetFlat', Commands.PresetFlat);
+  buildCachedButton('Reverie', mqtt, controller, 'PresetZeroG', Commands.PresetZeroG);
+  buildCachedButton('Reverie', mqtt, controller, 'PresetAntiSnore', Commands.PresetAntiSnore);
+  buildCachedButton('Reverie', mqtt, controller, 'PresetFlat', Commands.PresetFlat);
 
-  buildCachedButton('Richmat', mqtt, controller, 'PresetMemory1', Commands.PresetMemory1);
-  buildCachedButton('Richmat', mqtt, controller, 'PresetMemory2', Commands.PresetMemory2);
-  buildCachedButton('Richmat', mqtt, controller, 'PresetMemory3', Commands.PresetMemory3);
-  buildCachedButton('Richmat', mqtt, controller, 'PresetMemory4', Commands.PresetMemory4);
+  buildCachedButton('Reverie', mqtt, controller, 'PresetMemory1', Commands.PresetMemory1);
+  buildCachedButton('Reverie', mqtt, controller, 'PresetMemory2', Commands.PresetMemory2);
+  buildCachedButton('Reverie', mqtt, controller, 'PresetMemory3', Commands.PresetMemory3);
+  buildCachedButton('Reverie', mqtt, controller, 'PresetMemory4', Commands.PresetMemory4);
 
-  buildCachedButton('Richmat', mqtt, controller, 'ProgramMemory1', Commands.ProgramMemory1, 'config');
-  buildCachedButton('Richmat', mqtt, controller, 'ProgramMemory2', Commands.ProgramMemory2, 'config');
-  buildCachedButton('Richmat', mqtt, controller, 'ProgramMemory3', Commands.ProgramMemory3, 'config');
-  buildCachedButton('Richmat', mqtt, controller, 'ProgramMemory4', Commands.ProgramMemory4, 'config');
+  buildCachedButton('Reverie', mqtt, controller, 'ProgramMemory1', Commands.ProgramMemory1, 'config');
+  buildCachedButton('Reverie', mqtt, controller, 'ProgramMemory2', Commands.ProgramMemory2, 'config');
+  buildCachedButton('Reverie', mqtt, controller, 'ProgramMemory3', Commands.ProgramMemory3, 'config');
+  buildCachedButton('Reverie', mqtt, controller, 'ProgramMemory4', Commands.ProgramMemory4, 'config');
 };
