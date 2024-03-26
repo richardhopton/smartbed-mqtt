2b2f8e80532b3386f944011d46f82f9d633768e8
Richard Hopton <richard@thehoptons.com>
2024-03-26 13:35:30 -0700

    (Common) Don't allow invalid device topics

--- src/Common/buildMQTTDeviceData.ts
+++ src/Common/buildMQTTDeviceData.ts
@@ -1,10 +1,11 @@
 import { IDeviceData } from '@ha/IDeviceData';
+import { safeId } from '@utils/safeId';
 
 type Device = { friendlyName: string; name: string; address: number | string };
 
 export const buildMQTTDeviceData = ({ friendlyName, name, address }: Device, manufacturer: string): IDeviceData => {
   return {
-    deviceTopic: `${manufacturer.toLowerCase()}/${address}`,
+    deviceTopic: `${safeId(manufacturer)}/${safeId(address.toString())}`,
     device: {
       ids: [`${address}`],
       name: friendlyName,
