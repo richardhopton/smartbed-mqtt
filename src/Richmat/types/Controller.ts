eeb2a879408a4a7705f77610263303425632d299
Richard Hopton <richard@thehoptons.com>
2024-04-13 23:45:27 -0700

    Add support for repeating commands

--- src/Richmat/types/Controller.ts
+++ src/Richmat/types/Controller.ts
@@ -1,6 +1,7 @@
 import { IDeviceData } from '@ha/IDeviceData';
 import { Entity } from '@ha/base/Entity';
 import { Dictionary } from '@utils/Dictionary';
+import { Timer } from '@utils/Timer';
 import { wait } from '@utils/wait';
 import { IController } from 'Common/IController';
 import { IBLEDevice } from 'ESPHome/types/IBLEDevice';
@@ -13,6 +14,7 @@ export class Controller implements IController<number> {
   entities: Dictionary<Entity> = {};
   features: number;
   name: string;
+  private timer?: Timer = undefined;
 
   constructor(
     public deviceData: IDeviceData,
@@ -28,10 +30,24 @@ export class Controller implements IController<number> {
     return (this.features & feature) === feature;
   };
 
-  writeCommand = async (byte: number) => {
+  writeCommand = async (command: number, duration?: number, frequency?: number) => {
+    await this.timer?.cancel();
+
     await this.bleDevice.connect();
+    this.timer = new Timer(
+      async () => await this.write(command),
+      duration,
+      frequency,
+      async () => {
+        if (this.device.stayConnected) await this.bleDevice.disconnect();
+        this.timer = undefined;
+      }
+    );
+  };
+
+  private write = async (command: number) => {
     const { writeHandle, getBytes } = this.wrapper;
-    await this.bleDevice.writeCharacteristic(writeHandle, getBytes(byte));
+    await this.bleDevice.writeCharacteristic(writeHandle, getBytes(command));
     await wait(50);
     await this.bleDevice.writeCharacteristic(writeHandle, getBytes(0x6e));
     if (!this.device.stayConnected) await this.bleDevice.disconnect();
