d84ec753ce549de8b3f1c59178d6566df931c9c5
Richard Hopton <richard@thehoptons.com>
2024-09-10 11:37:55 -0700

    (Richmat) Add support for W6RM remote code

--- src/Richmat/Commands.ts
+++ src/Richmat/Commands.ts
@@ -1,11 +1,17 @@
 export enum Commands {
   PresetFlat = 0x31,
   PresetAntiSnore = 0x46,
+  PresetLounge = 0x59,
   PresetMemory1 = 0x2e,
+  PresetMemory2 = 0x2f,
+  PresetTV = 0x58,
   PresetZeroG = 0x45,
 
   ProgramAntiSnore = 0x69,
+  ProgramLounge = 0x65,
   ProgramMemory1 = 0x2b,
+  ProgramMemory2 = 0x2c,
+  ProgramTV = 0x64,
   ProgramZeroG = 0x66,
 
   MassageHeadStep = 0x4c,
