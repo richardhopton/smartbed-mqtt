15a587083f3872868c8c2b024506f61c89171059
Richard Hopton <richard@thehoptons.com>
2024-09-22 17:32:40 -0700

    (Linak) Prep for motor control

--- src/Linak/Commands.ts
+++ src/Linak/Commands.ts
@@ -1,3 +1,8 @@
+export enum Direction {
+  UP,
+  DOWN,
+}
+
 export const Commands = {
   PresetMemory1: [0xe, 0x0],
   PresetMemory2: [0xf, 0x0],
@@ -27,4 +32,63 @@ export const Commands = {
   MassageFootDown: [0x90, 0x0],
 
   MassageModeStep: [0x81, 0x0],
+
+  Move: (head?: Direction, back?: Direction, legs?: Direction, feet?: Direction) => {
+    if (head == Direction.UP && back == Direction.UP && legs == Direction.UP && feet == Direction.UP)
+      return [0x00, 0x00];
+    if (head == Direction.DOWN && back == Direction.DOWN && legs == Direction.DOWN && feet == Direction.DOWN)
+      return [0x01, 0x00];
+
+    if (legs == Direction.UP) {
+      if (back == Direction.UP) return [0x37, 0x00];
+      if (back == Direction.DOWN) return [0x36, 0x00];
+    }
+    if (legs == Direction.DOWN) {
+      if (back == Direction.UP) return [0x35, 0x00];
+      if (back == Direction.DOWN) return [0x34, 0x00];
+    }
+
+    if (head == Direction.UP) {
+      if (back == Direction.UP) return [0x33, 0x00];
+      if (legs == Direction.UP) return [0x32, 0x00];
+      if (back == Direction.DOWN) return [0x31, 0x00];
+      if (legs == Direction.DOWN) return [0x30, 0x00];
+    }
+    if (head == Direction.DOWN) {
+      if (back == Direction.UP) return [0x2f, 0x00];
+      if (legs == Direction.UP) return [0x2e, 0x00];
+      if (back == Direction.DOWN) return [0x2d, 0x00];
+      if (legs == Direction.DOWN) return [0x2c, 0x00];
+    }
+
+    if (feet == Direction.UP) {
+      if (back == Direction.UP) return [0x2b, 0x00];
+      if (legs == Direction.UP) return [0x2a, 0x00];
+      if (head == Direction.UP) return [0x29, 0x00];
+      if (back == Direction.DOWN) return [0x28, 0x00];
+      if (legs == Direction.DOWN) return [0x27, 0x00];
+      if (head == Direction.DOWN) return [0x26, 0x00];
+    }
+
+    if (feet == Direction.DOWN) {
+      if (back == Direction.UP) return [0x25, 0x00];
+      if (legs == Direction.UP) return [0x24, 0x00];
+      if (head == Direction.UP) return [0x23, 0x00];
+      if (back == Direction.DOWN) return [0x22, 0x00];
+      if (legs == Direction.DOWN) return [0x21, 0x00];
+      if (head == Direction.DOWN) return [0x20, 0x00];
+    }
+
+    if (back == Direction.UP) return [0x0b, 0x00];
+    if (back == Direction.DOWN) return [0x0a, 0x00];
+    if (legs == Direction.UP) return [0x09, 0x00];
+    if (legs == Direction.DOWN) return [0x08, 0x00];
+
+    if (feet == Direction.UP) return [0x05, 0x00];
+    if (feet == Direction.DOWN) return [0x04, 0x00];
+    if (head == Direction.UP) return [0x03, 0x00];
+    if (head == Direction.DOWN) return [0x02, 0x00];
+
+    return [0xff, 0x00];
+  },
 };
