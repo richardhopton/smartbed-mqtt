4b372e5c75c21e2ee705d3f99f384a7eaaaadc66
Richard Hopton <richard@thehoptons.com>
2025-01-21 21:56:38 -0800

    (Common) Simplify message sending to reduce bugs

--- src/BLE/BLEController.ts
+++ src/BLE/BLEController.ts
@@ -15,6 +15,7 @@ export class BLEController<TCommand> extends EventEmitter implements IEventSourc
   }
   private timer?: Timer = undefined;
   private notifyValues: Dictionary<Uint8Array> = {};
+  private disconnectTimeout?: NodeJS.Timeout = undefined;
 
   constructor(
     public deviceData: IDeviceData,
@@ -35,30 +36,35 @@ export class BLEController<TCommand> extends EventEmitter implements IEventSourc
     });
   }
 
-  writeCommand = (command: TCommand, count?: number, waitTime?: number) =>
+  private disconnect = () => this.bleDevice.disconnect();
+
+  private write = async (command: number[]) => {
+    if (this.disconnectTimeout) this.disconnectTimeout = undefined;
+    await this.bleDevice.writeCharacteristic(this.handle, new Uint8Array(command));
+    if (this.stayConnected) return;
+
+    this.disconnectTimeout = setTimeout(this.disconnect, 60_000);
+  };
+
+  writeCommand = (command: TCommand, count: number = 1, waitTime?: number) =>
     this.writeCommands([command], count, waitTime);
 
-  writeCommands = async (commands: TCommand[], count?: number, waitTime?: number) => {
+  writeCommands = async (commands: TCommand[], count: number = 1, waitTime?: number) => {
     const commandList = commands.map(this.commandBuilder).filter((command) => command.length > 0);
     if (commandList.length === 0) return;
-    await this.timer?.cancel();
+
+    await this.cancelCommands();
     await this.bleDevice.connect();
-    const onFinish = async () => {
-      if (!this.stayConnected) await this.bleDevice.disconnect();
+
+    const onTick =
+      commandList.length === 1 ? () => this.write(commandList[0]) : () => loopWithWait(commandList, this.write);
+    if (count === 1) return onTick();
+
+    const onFinish = () => {
       this.timer = undefined;
     };
-    this.timer = new Timer(
-      () =>
-        loopWithWait(commandList, (command) =>
-          this.bleDevice.writeCharacteristic(this.handle, new Uint8Array(command))
-        ),
-      {
-        count,
-        waitTime,
-        onFinish,
-      }
-    );
-    await this.timer.done;
+    this.timer = new Timer(onTick, count, waitTime, onFinish);
+    await this.timer.start();
   };
 
   cancelCommands = async () => {
--- src/ErgoWifi/Controller.ts
+++ src/ErgoWifi/Controller.ts
@@ -42,25 +42,27 @@ export class Controller implements IController<number> {
 
   constructor(public deviceData: IDeviceData, public device: Device, public user: Credentials) {}
 
-  writeCommand = async (command: number, count?: number, waitTime?: number) =>
+  writeCommand = async (command: number, count: number = 1, waitTime?: number) =>
     this.writeCommands([command], count, waitTime);
 
-  writeCommands = async (commands: number[], count?: number, waitTime?: number) => {
-    await this.timer?.cancel();
+  writeCommands = async (commands: number[], count: number = 1, waitTime?: number) => {
+    await this.cancelCommands();
 
     const authDetails = await getAuthDetails(this.user);
     if (!authDetails) return;
 
     const { userId, authorize } = authDetails;
     const socket = await getConnection((socket) => socket.write(loginPayload(userId, authorize)));
-    this.timer = new Timer(
-      () => loopWithWait(commands, async (command) => await socket.write(commandPayload(this.device.id, command))),
-      {
-        count,
-        waitTime,
-        onFinish: () => (this.timer = undefined),
-      }
-    );
+    const write = (command: number) => socket.write(commandPayload(this.device.id, command));
+
+    const onTick = commands.length === 1 ? () => write(commands[0]) : () => loopWithWait(commands, write);
+    if (count === 1) return onTick();
+
+    const onFinish = () => {
+      this.timer = undefined;
+    };
+    this.timer = new Timer(onTick, count, waitTime, onFinish);
+    await this.timer.start();
   };
 
   cancelCommands = async () => {
