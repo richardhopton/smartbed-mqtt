ddddb8a048a6f9b481d463fda210bc6b7f46e017
Richard Hopton <richard@thehoptons.com>
2025-01-26 23:40:37 -0800

    (BLE) Correctly handle event handlers

--- src/Utils/arrayEquals.ts
+++ src/Utils/arrayEquals.ts
@@ -1,2 +1,2 @@
-export const arrayEquals = (arr1: number[], arr2: number[]) =>
+export const arrayEquals = (arr1: number[] | Uint8Array, arr2: number[] | Uint8Array) =>
   arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
