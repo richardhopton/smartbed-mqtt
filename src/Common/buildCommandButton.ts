da18db5ca7e3f58563045830878eebc24b6b4106
Richard Hopton <richard@thehoptons.com>
2025-02-01 22:00:18 -0800

    Remove unnecessary parameters

--- src/Common/buildCommandButton.ts
+++ src/Common/buildCommandButton.ts
@@ -11,15 +11,13 @@ export const buildCommandButton = <TCommand>(
   { cache, deviceData, writeCommand }: IController<TCommand>,
   name: StringsKey,
   command: TCommand,
-  category?: string,
-  count?: number,
-  waitTime?: number
+  category?: string
 ) => {
   if (cache[name]) return;
 
   cache[name] = new Button(mqtt, deviceData, buildEntityConfig(name, category), async () => {
     try {
-      await writeCommand(command, count, waitTime);
+      await writeCommand(command);
     } catch (e) {
       logError(`[${context}] Failed to write '${getString(name)}'`, e);
     }
